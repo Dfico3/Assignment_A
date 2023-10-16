63a112fbbef760031d290601ab29918ef37bc9d9
Dfico3 <145590007+Dfico3@users.noreply.github.com>
2023-10-16 13:29:57 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        public void print(String filename, boolean doubleSided)
+        public int getStatus(int delay)
+
+    }
+}
